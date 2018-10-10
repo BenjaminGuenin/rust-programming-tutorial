1c2bd68e28b9b496e1604e619e56fab798015000
Light_Celestial <lightcelestial55555@gmail.com>
2018-10-10 08:35:29 +0200

    Add multiple guess and handling error

--- guessing-game/guessing_game/src/main.rs
+++ guessing-game/guessing_game/src/main.rs
@@ -1,10 +1,44 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
-fn main() {
+fn main()
+{
     println!("Guess the number!");
-    println!("Enter your input...");
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess);
-    println!("You guessed {}", guess);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
+    loop
+    {
+        println!("Enter your input...");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // Switching from an expect call to a match expression is how you generally move from crashing on an error to handling the error
+        let guess: u32 = match guess.trim().parse()
+        {
+            // Remember that parse returns a Result type and Result is an enum that has the variants Ok or Err
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number)
+        {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You won!");
+                break;
+            }
+        }
+
+        println!("You guessed {}", guess);
+    }
 }
